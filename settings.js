edf0da1f1024034878a11cb60e72568248cfd7c2
ACREY HAULE <hauleacrey1@gmail.com>
2025-02-20 21:12:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~JlQRHQ6A#WhYswDiED_bDpMgfr_LvZ2CVaRH4wIkkOIdluE7K7iI",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
